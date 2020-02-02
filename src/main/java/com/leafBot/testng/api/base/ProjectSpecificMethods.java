34e1b53078c4ac3d8d1072fad892e0b6c241e870
Aravind <Aravind@Aravind>
2020-02-02 20:53:53 +0530

    Added a method

--- src/main/java/com/leafBot/testng/api/base/ProjectSpecificMethods.java
+++ src/main/java/com/leafBot/testng/api/base/ProjectSpecificMethods.java
@@ -2,7 +2,6 @@ package com.leafBot.testng.api.base;
 
 import java.io.IOException;
 
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
@@ -11,9 +10,8 @@ import com.leafBot.selenium.api.base.SeleniumBase;
 
 import utils.DataLibrary;
 
-
 public class ProjectSpecificMethods extends SeleniumBase {
-
+    
 	public String dataSheetName;		
 
 	@DataProvider(name = "fetchData")
